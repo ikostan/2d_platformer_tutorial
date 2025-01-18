7614561dc7a12afd76137a71b344f1190c7b4edc
Egor Kostan <igorkostan@gmail.com>
2025-01-17 21:59:20 -0800

    Remove constants

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -4,9 +4,6 @@ class_name Player extends CharacterBody2D
 @export var speed: float = 200.0
 @export var gravity: float = 980.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -18,8 +15,8 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction: float = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
