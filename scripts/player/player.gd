9cceecaf409bd22283b65b267365efe11c4b3cb0
Egor Kostan <igorkostan@gmail.com>
2025-01-19 20:48:18 -0800

    keyboard mapping

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -12,7 +12,7 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction: float = Input.get_axis("ui_left", "ui_right")
+	var direction: float = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * speed
 	else:
