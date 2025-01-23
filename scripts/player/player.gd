2b324a8495fe37cbb028098c77a59d3fcce5a8aa
Egor Kostan <igorkostan@gmail.com>
2025-01-22 21:21:01 -0800

    jUMP IMPLEMENTATION

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -6,39 +6,50 @@ class_name Player extends CharacterBody2D
 @export var speed: float = 200.0
 @export var air_speed: float = 150.0
 @export var air_acceleration: float = 0.1
-@export var jump_force: float = -400.0
+@export var air_deceleration: float = 0.5
+@export var jump_force: float = -300.0
 @export var gravity: float = 980.0
 
 var direction: float
 
+
 func _process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	direction = Input.get_axis("left", "right")
+	if direction:
+		# flip animation direction left/right
+		sprite_2d.flip_h = direction < 0
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		handle_air_movement(delta)
+		handle_air_movement()
 	else:
-		handle_ground_movement(delta)
-		
+		handle_ground_movement()
+	
+	handle_jump_movement()
 	update_animation()
 	move_and_slide()
 
 
-func handle_air_movement(delta) -> void:
+func handle_air_movement() -> void:
+	# lerp: Vector interpolation
 	if direction:
-		# flip animation direction left/right
-		sprite_2d.flip_h = direction < 0
+		velocity.x = lerp(velocity.x, direction * air_speed, air_acceleration)
+	else:
+		velocity.x = lerp(velocity.x, 0.0, air_deceleration)
 
 
-func handle_ground_movement(delta: float) -> void:
+func handle_jump_movement() -> void:
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_force
+
+
+func handle_ground_movement() -> void:
 	if direction:
-		velocity.x = (direction * speed) + delta
-		# flip animation direction left/right
-		sprite_2d.flip_h = direction < 0
+		velocity.x = (direction * speed)
 	else:
 		# No direction, stop player slowly
 		velocity.x = move_toward(velocity.x, 0, speed)
