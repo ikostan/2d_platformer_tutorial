92f7c16d94635885eb78b779033806819852a40d
Egor Kostan <igorkostan@gmail.com>
2025-01-22 21:54:12 -0800

    Update scene_script.gd

--- scripts/levels/scene_script.gd
+++ scripts/levels/scene_script.gd
@@ -2,7 +2,7 @@ class_name Level extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	RenderingServer.set_default_clear_color("00003c")
+	RenderingServer.set_default_clear_color(Color("00003c"))
 	if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_WINDOWED:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 	print("WINDOW_MODE: ", DisplayServer.window_get_mode())
