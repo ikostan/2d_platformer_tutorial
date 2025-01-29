cf9d1b12bedf2d25939b5b4f2e7abc14775c9a72
Egor Kostan <igorkostan@gmail.com>
2025-01-28 21:43:33 -0800

    Update test_player_script.gd

--- test/test_player_script.gd
+++ test/test_player_script.gd
@@ -4,20 +4,19 @@ var Scene = preload("res://scenes/testing_scene/test.tscn")
 var scene
 var player
 var _sender = InputSender.new(Input)
+var _start_point_x: float = 334.0
 
 
 func before_each() -> void:
 	scene = add_child_autofree(Scene.instantiate())
-	player = add_child_autofree(scene.get_node("Player"))
+	player = scene.get_node("Player")
 	scene.get_tree().process_frame
-
 
 func after_each() -> void:
 	scene.queue_free()
 	player.queue_free()
 	_sender.release_all()
 	_sender.clear()
-
 
 func test_when_uai_enabled_input_not_processed_immediately():
 	_sender.key_down('a')
@@ -52,17 +51,17 @@ func test_when_uai_enabled_flushing_buffer_just_pressed_is_processed_immediately
 	Input.flush_buffered_events()
 	assert_true(Input.is_action_just_pressed('left'))
 	
-# In this test we press and hold the left button for .3 seconds then wait
+# In this test we press and hold the left button for .1 seconds then wait
 # another .3 seconds for the left to take take place.  We then assert that
-# the character has moved left between 15 and 18 pixels.
+# the character has moved left arround 25 pixels.
 func test_tapping_left_changes_certain_lenght():
-	#var player = add_child_autofree(Player.new())
-	_sender.action_down("left").hold_for(.3).wait(.3)
+	_sender.wait(1.0)
+	_sender.action_down("left").hold_for(.1).wait(.3)
 	await(_sender.idle)
-	assert_between(player.position.x, 315.0, 333.0)
+	assert_between(player.position.x, _start_point_x - 25.0, _start_point_x - 1.0)
 	
 func test_tapping_right_changes_certain_lenght():
-	#var player = add_child_autofree(Player.new())
-	_sender.action_down("right").hold_for(.3).wait(.3)
+	_sender.wait(1.0)
+	_sender.action_down("right").hold_for(.1).wait(.3)
 	await(_sender.idle)
-	assert_between(player.position.x, 334.0, 365.0)
+	assert_between(player.position.x, _start_point_x + 1, _start_point_x + 25.0)
